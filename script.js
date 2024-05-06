22b530f6587001923ec61c4b6ead06b399b7eadc
QuitasolKenlei <146052692+QuitasolKenlei@users.noreply.github.com>
2024-05-06 12:49:25 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+
+let name = "Quitasol, Kenlei A.";
+let address = "Quinabalayangan Sta. Lucia Ilocos Sur";
+let age = 20;
+let classRole = "Student";
+
+const COURSE = "BSCS";
+
+if (COURSE === "BSCS") {
+
+  if (classRole === "Student") {
+    let password = prompt("Enter your password:");
+    if (password.length >= 12 && password.includes(COURSE)) {
+      console.log("Name:", name);
+      console.log("Address:");
+      for (let i = 0; i < age; i++) {
+        console.log(address);
+      }
+    } else {
+      console.log("Password does not meet the requirements.");
+    }
+  } else {
+    console.log("Invalid class role.");
+  }
+} else {
+  console.log("Invalid course.");
+}
